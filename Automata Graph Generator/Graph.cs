0f95791d5dee1a93c466ca945593d29ec931b3e8
Filip Stolarczuk <fifiak1001@gmail.com>
2017-10-30 22:48:25 +0100

    Added some documentation

--- Automata Graph Generator/Graph.cs
+++ Automata Graph Generator/Graph.cs
@@ -46,6 +46,15 @@ namespace Automata_Graph_Generator
             return false;
         }
 
+        /// <summary>
+        /// Adds a state to the graph.
+        /// </summary>
+        /// <param name="name">Name of the state.</param>
+        /// <param name="accepted">Defines if the state is accepted.</param>
+        /// <param name="previousStateName">Name of one of the states that has the transition to the new state.</param>
+        /// <param name="previousTransition">Symbol under which the automata goes from previousState to the new state.</param>
+        /// <param name="transitions">Transitions from the new state ['symbol', "nextStateName"].</param>
+        /// <returns></returns>
         public override bool AddState(string name, bool accepted, string previousStateName, char previousTransition, Dictionary<char, Node> transitions = null)
         {
             if (!AllStates.Where(n => n.Name == name).Any())
